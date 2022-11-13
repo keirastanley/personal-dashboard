cdf13f9c99a5444ecb41ddbef0157405116d45db
keirastanley <keiraestanley@gmail.com>
2022-11-13 12:29:55 +0000

    Weather widget changed

--- my-app/src/Components/Weather/index.js
+++ my-app/src/Components/Weather/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css"
 
 function Weather(){
-    return <div id="weather"><iframe title="weather" src="https://www.yourweather.co.uk/getwid/3e04663e5963fdd6a6c8db479db7a582" id="3e04663e5963fdd6a6c8db479db7a582" name = "flipe"></iframe></div>
+    return <a class="weatherwidget-io" href="https://forecast7.com/en/51d51n0d13/london/" data-label_1="London" data-label_2="WEATHER" data-days="5" data-theme="pure" >London WEATHER<iframe id="weatherwidget-io-0" class="weatherwidget-io-frame" title="Weather Widget" scrolling="no" frameBorder="0" width="100%" src="https://weatherwidget.io/w/"></iframe></a>
 }
 
 export default Weather;
