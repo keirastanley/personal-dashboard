41dce35b1c95bbc964315db2b2680310306a062a
keirastanley <keiraestanley@gmail.com>
2022-11-17 13:23:21 +0000

    Refresh functionality added to poetry section

--- my-app/src/Components/Poetry/index.js
+++ my-app/src/Components/Poetry/index.js
@@ -3,12 +3,14 @@ import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { GoBook } from "react-icons/go";
 import { TbRefresh } from "react-icons/tb";
 import "./index.css";
+import poems from "./poems";
 
 function Poetry() {
   const [toggle, setToggle] = useState(false);
   const [heart, setHeart] = useState(<AiOutlineHeart id="heart-icon" />);
+  const [id, setId] = useState(0)
 
-  function handleClick() {
+  function handleHeart() {
     setToggle(!toggle);
     if (toggle === true) {
       setHeart(<AiFillHeart id="poetry-heart-icon-filled" />);
@@ -17,31 +19,30 @@ function Poetry() {
     }
   }
 
+  function handleRefresh(){
+    const randomNumber = Math.floor(Math.random() * poems.length);
+    setId(randomNumber)
+  }
+
+  function Lines(){
+    const lines = poems[id].lines.filter((line, ind, arr) => ind < 4);
+    return lines.map(line => <p className="lines">{line}</p>)
+  }
+
   return (
     <div className="poetry-container">
         <div className="poetry-info">
-        <div className="poetry-text">
-            <p>
-            Ye lovers of the picturesque, away and see
-            <br></br>
-            Beautiful Balmoral, near by
-            the River Dee;
-            <br></br>
-            There ye will see the deer browsing on the heathery
-            hills
-            <br></br>
-            While adown their sides run clear sparkling rills.
-            <br></br>
-            <i>From Beautiful Balmoral by William Topaz McGonagall</i>
-            </p>
-      </div>
+          <div className="poetry-text">
+            <Lines/>
+            <p>From <i>{poems[id].title}</i> by <i>{poems[id].author}</i></p>
+        </div>
       <div className="poetry-icons">
-        <button onClick={handleClick}>{heart}</button>
+        <button onClick={handleHeart}>{heart}</button>
         <button>
           <GoBook id="poetry-book-icon" />
         </button>
         <button>
-          <TbRefresh id="poetry-refresh-icon" />
+          <TbRefresh id="poetry-refresh-icon" onClick={handleRefresh} />
         </button>
       </div>
         </div>
